5b79e7a4ff43113c0e8f1ac33015fd41c0bfc8c2
karix <eivanov200606@mail.ru>
2025-04-29 22:17:54 +0700

    add signup

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Model\Post;
+use Model\User;
 use Src\View;
 use Src\Request;
 
@@ -19,4 +20,13 @@ class Site
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
