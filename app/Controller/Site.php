f243611dac5a3d5f73af2ed8c3430b1d92956043
karix <eivanov200606@mail.ru>
2025-05-05 22:29:32 +0700

    validators

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Model\Post;
 use Model\User;
 use Src\Route;
+use Src\Validator\Validator;
 use Src\Auth\Auth;
 use Src\View;
 use Src\Request;
@@ -25,8 +26,25 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method === 'POST' && User::create($request->all())) {
-            app()->route->redirect('/go');
+        if ($request->method === 'POST') {
+
+            $validator = new Validator($request->all(), [
+                'name' => ['required'],
+                'login' => ['required', 'unique:users,login'],
+                'password' => ['required']
+            ], [
+                'required' => 'Поле :field пусто',
+                'unique' => 'Поле :field должно быть уникально'
+            ]);
+
+            if($validator->fails()){
+                return new View('site.signup',
+                    ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+            }
+
+            if (User::create($request->all())) {
+                app()->route->redirect('/login');
+            }
         }
         return new View('site.signup');
     }
