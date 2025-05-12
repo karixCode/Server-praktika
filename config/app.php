6192781e0c8a35476f769b21fcf97960a190f6cb
karix <eivanov200606@mail.ru>
2025-05-12 11:41:57 +0700

    api

--- config/app.php
+++ config/app.php
@@ -8,13 +8,22 @@ return [
     'routeMiddleware' => [
         'auth' => \Middlewares\AuthMiddleware::class,
     ],
+    //Классы для middleware
     'routeAppMiddleware' => [
         'csrf' => \Middlewares\CSRFMiddleware::class,
-        'trim' => \Middlewares\TrimMiddleware::class,
         'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
     ],
     'validators' => [
         'required' => \Validators\RequireValidator::class,
         'unique' => \Validators\UniqueValidator::class
-    ]
+    ],
+    //Классы провайдеров
+    'providers' => [
+        'kernel' => Providers\KernelProvider::class,
+        'route' => Providers\RouteProvider::class,
+        'db' => Providers\DBProvider::class,
+        'auth' => Providers\AuthProvider::class,
+    ],
 ];
